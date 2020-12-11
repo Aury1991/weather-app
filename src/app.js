e863f1b66fe5543df7bfc687e2c1eb9b9e0e6789
Aury1991 <70163335+Aury1991@users.noreply.github.com>
2020-12-11 14:35:05 +0100

    current temperature

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ function displayTemperature(response){
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
     let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].main;
+    humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=Math.round(response.data.wind.speed);
 }
 let apikey="f4a18a1f990c901b70bdae86bb1efc99";
 let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${apikey}&units=metric`;
